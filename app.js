497d1492a466074d668d7f737e75942179d729c1
Zachary Kuchar <zacharyk323@live.com>
2023-06-13 20:26:46 -0400

    Add toggle functions

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ const ani2 = () => {};
 
 const ani3 = () => {};
 
+const catToggle = () => {
+  let cat = document.querySelector(".Cat");
+  cat.classList.toggle("chosen");
+};
+
 const dogToggle = () => {
   let dog = document.querySelector(".Dog");
   dog.classList.toggle("chosen");
 };
 
-const toggle2 = () => {
-  document.querySelector(".Cat");
-};
-
-const toggle3 = () => {
-  document.querySelector(".Rabbit");
+const rabbitToggle = () => {
+  let rabbit = document.querySelector(".Rabbit");
+  rabbit.classList.toggle("chosen");
 };
