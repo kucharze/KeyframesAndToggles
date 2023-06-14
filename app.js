9b2ae0e9ad49e99f44c615196594759e7e679645
Zachary Kuchar <zacharyk323@live.com>
2023-06-13 20:07:14 -0400

    Made dog toggle class

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const ani2 = () => {};
 
 const ani3 = () => {};
 
-const toggle1 = () => {};
+const dogToggle = () => {
+  let dog = document.querySelector(".Dog");
+  dog.classList.toggle("chosen");
+};
 
-const toggle2 = () => {};
+const toggle2 = () => {
+  document.querySelector(".Cat");
+};
+
+const toggle3 = () => {
+  document.querySelector(".Rabbit");
+};
