c7057836e5a0373ce8c2e903ba5f1af43b14beca
Zachary Kuchar <zacharyk323@live.com>
2023-06-13 21:12:05 -0400

    Added top section animations

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
-const ani1 = () => {};
+const dog = () => {
+  let dog = document.querySelector(".dog");
 
-const ani2 = () => {};
+  if (dog.classList.contains("bigger")) {
+    dog.classList.toggle("smaller");
+    dog.classList.toggle("bigger");
+  } else if (dog.classList.contains("smaller")) {
+    dog.classList.toggle("bigger");
+    dog.classList.toggle("smaller");
+  } else {
+    dog.classList.toggle("bigger");
+  }
+};
 
-const ani3 = () => {};
+const cat = () => {
+  let cat = document.querySelector(".cat");
+  if (cat.classList.contains("bigger")) {
+    cat.classList.toggle("smaller");
+    cat.classList.toggle("bigger");
+  } else if (cat.classList.contains("smaller")) {
+    cat.classList.toggle("bigger");
+    cat.classList.toggle("smaller");
+  } else {
+    cat.classList.toggle("bigger");
+  }
+};
+
+const rabit = () => {
+  let rabit = document.querySelector(".rabit");
+  // rabit.classList.toggle("bigger");
+
+  if (rabit.classList.contains("bigger")) {
+    rabit.classList.toggle("smaller");
+    rabit.classList.toggle("bigger");
+    // console.log("rabit bigger");
+  } else if (rabit.classList.contains("smaller")) {
+    rabit.classList.toggle("bigger");
+    rabit.classList.toggle("smaller");
+  } else {
+    rabit.classList.toggle("bigger");
+  }
+};
 
 const catToggle = () => {
   let dog = document.querySelector(".Dog");
