16481503fc25b04758a8ec8517fd6137e1192948
Zachary Kuchar <zacharyk323@live.com>
2023-06-13 20:49:48 -0400

    Completed bottom animation and toggle section

--- app.js
+++ app.js
@@ -5,16 +5,46 @@ const ani2 = () => {};
 const ani3 = () => {};
 
 const catToggle = () => {
+  let dog = document.querySelector(".Dog");
   let cat = document.querySelector(".Cat");
+  let rabbit = document.querySelector(".Rabbit");
+  if (rabbit.classList.contains("chosen")) {
+    rabbit.classList.toggle("chosen");
+  }
+  if (dog.classList.contains("chosen")) {
+    dog.classList.toggle("chosen");
+  }
   cat.classList.toggle("chosen");
+  document.querySelector(".cast").innerHTML = "Your vote has been cast";
 };
 
 const dogToggle = () => {
   let dog = document.querySelector(".Dog");
+  let cat = document.querySelector(".Cat");
+  let rabbit = document.querySelector(".Rabbit");
+
+  if (rabbit.classList.contains("chosen")) {
+    rabbit.classList.toggle("chosen");
+  }
+  if (cat.classList.contains("chosen")) {
+    cat.classList.toggle("chosen");
+  }
+
   dog.classList.toggle("chosen");
+  document.querySelector(".cast").innerHTML = "Your vote has been cast";
 };
 
 const rabbitToggle = () => {
   let rabbit = document.querySelector(".Rabbit");
+  let dog = document.querySelector(".Dog");
+  let cat = document.querySelector(".Cat");
+
+  if (dog.classList.contains("chosen")) {
+    dog.classList.toggle("chosen");
+  }
+  if (cat.classList.contains("chosen")) {
+    cat.classList.toggle("chosen");
+  }
   rabbit.classList.toggle("chosen");
+  document.querySelector(".cast").innerHTML = "Your vote has been cast";
 };
