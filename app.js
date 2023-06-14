a608b13365e8b54674c001230a0524fc803cd1d9
Zachary Kuchar <zacharyk323@live.com>
2023-06-13 21:17:05 -0400

    Added show functions

--- app.js
+++ app.js
@@ -4,11 +4,23 @@ const dog = () => {
   if (dog.classList.contains("bigger")) {
     dog.classList.toggle("smaller");
     dog.classList.toggle("bigger");
+    dogShow(false);
   } else if (dog.classList.contains("smaller")) {
     dog.classList.toggle("bigger");
     dog.classList.toggle("smaller");
+    dogShow(true);
   } else {
     dog.classList.toggle("bigger");
+    dogShow(true);
+  }
+};
+
+const dogShow = (status) => {
+  let dog = document.querySelector(".dogselection");
+  if (status) {
+    dog.innerHTML = "hello";
+  } else {
+    dog.innerHTML = "";
   }
 };
 
@@ -17,11 +29,23 @@ const cat = () => {
   if (cat.classList.contains("bigger")) {
     cat.classList.toggle("smaller");
     cat.classList.toggle("bigger");
+    catShow(false);
   } else if (cat.classList.contains("smaller")) {
     cat.classList.toggle("bigger");
     cat.classList.toggle("smaller");
+    catShow(true);
   } else {
     cat.classList.toggle("bigger");
+    catShow(true);
+  }
+};
+
+const catShow = (status) => {
+  let cat = document.querySelector(".catselection");
+  if (status) {
+    cat.innerHTML = "hello";
+  } else {
+    cat.innerHTML = "";
   }
 };
 
@@ -32,12 +56,24 @@ const rabit = () => {
   if (rabit.classList.contains("bigger")) {
     rabit.classList.toggle("smaller");
     rabit.classList.toggle("bigger");
+    rabbitShow(false);
     // console.log("rabit bigger");
   } else if (rabit.classList.contains("smaller")) {
     rabit.classList.toggle("bigger");
     rabit.classList.toggle("smaller");
+    rabbitShow(true);
   } else {
     rabit.classList.toggle("bigger");
+    rabbitShow(true);
+  }
+};
+
+const rabbitShow = (status) => {
+  let rabit = document.querySelector(".rabbitselection");
+  if (status) {
+    rabit.innerHTML = "hello";
+  } else {
+    rabit.innerHTML = "";
   }
 };
 
